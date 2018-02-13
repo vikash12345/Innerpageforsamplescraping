20e10f658e70092a7d9871127fa1e52581df1069
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-02-13 15:57:37 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -7,10 +7,10 @@ require 'scraperwiki/simple_html_dom.php';
 //              
                   $urlofpage  = 'https://indiankanoon.org/search/?formInput=doctypes:bombay%20fromdate:1-12-1902%20todate:%2031-12-1902';
                   $pagination	=	$urlofpage.'&pagenum=';
-          	echo   $linkofpage	=	file_get_html($pagination);
+          	  $linkofpage	=	file_get_html($pagination);
 if($linkofpage){		  
 			$NoMATCH	=	$linkofpage->find("//b[plaintext^=No matching results]", 0)->plaintext;
-		  	$Next			=	$linkofpage->find("//a[plaintext^=Next]", 0)->plaintext;
+		  	echo $Next			=	$linkofpage->find("//a[plaintext^=Next]", 0)->plaintext;
 			$var	=	0;		
         	while($var >= 39)
 			{
