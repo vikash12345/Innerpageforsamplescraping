a382ae9e8301ba522e14c5120157fd1eaa9f0b81
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-02-13 11:57:30 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -7,7 +7,7 @@ require 'scraperwiki/simple_html_dom.php';
 //              
                   $urlofpage  = 'https://indiankanoon.org/search/?formInput=doctypes:supremecourt fromdate:1-1-1948 todate: 31-1-1948';
                   $pagination	=	$urlofpage.'&pagenum=';
-          				$linkofpage	=	file_get_tml($pagination);
+          				$linkofpage	=	file_get_html($pagination);
         				if($linkofpage)
 					{
 					    echo $linkofpage;
