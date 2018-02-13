759e8125ca80cea5ccfe69480cb014c016dfead5
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-02-13 11:56:29 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -4,24 +4,17 @@
 
 // require 'scraperwiki.php';
 // require 'scraperwiki/simple_html_dom.php';
+//              
+                  $urlofpage  = 'https://indiankanoon.org/search/?formInput=doctypes:supremecourt fromdate:1-1-1948 todate: 31-1-1948';
+                  $pagination	=	$urlofpage.'&pagenum=';
+          				$linkofpage	=	file_get_tml($pagination);
+        				if($linkofpage)
+					{
+					    echo $linkofpage;
+					}
+scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
 //
-// // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
-//
-// // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
-//
-// // Write out to the sqlite database using scraperwiki library
-// scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
-//
-// // An arbitrary query against the database
-// scraperwiki::select("* from data where 'name'='peter'")
+scraperwiki::select("* from data where 'name'='peter'")
 
-// You don't have to do things with the ScraperWiki library.
-// You can use whatever libraries you want: https://morph.io/documentation/php
-// All that matters is that your final data is written to an SQLite database
-// called "data.sqlite" in the current working directory which has at least a table
-// called "data".
+
 ?>
