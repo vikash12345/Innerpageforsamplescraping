cfb4cddf510c0513e61ac7a11a4a3c5ece591722
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-02-13 11:59:32 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -5,7 +5,7 @@
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 //              
-                  $urlofpage  = 'https://indiankanoon.org/search/?formInput=doctypes:supremecourt%20fromdate:1-1-1948%20todate:%2031-1-1948';
+                  $urlofpage  = 'https://indiankanoon.org/search/?formInput=doctypes:supremecourt%20fromdate:1-4-1947%20todate:%2030-4-1947';
                   $pagination	=	$urlofpage.'&pagenum=';
           				$linkofpage	=	file_get_html($pagination);
         				if($linkofpage)
