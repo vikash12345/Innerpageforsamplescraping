ef73c5e5a131854026d89e2865e9acb1bb5b2951
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-02-13 16:16:19 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -8,21 +8,17 @@ require 'scraperwiki/simple_html_dom.php';
                   $urlofpage  = 'https://indiankanoon.org/search/?formInput=doctypes:bombay%20fromdate:1-12-1902%20todate:%2031-12-1902';
                   $pagination	=	$urlofpage.'&pagenum=';
           	  $linkofpage	=	file_get_html($pagination);
-if($linkofpage){		  
+		 if($linkofpage)
+		 {	
+ 		  	$Next		=	$linkofpage->find("//a[plaintext^=Next]", 0)->plaintext;
 			$NoMATCH	=	$linkofpage->find("//b[plaintext^=No matching results]", 0)->plaintext;
-		  	$Next		=	$linkofpage->find("//a[plaintext^=Next]", 0)->plaintext;
 			$var	=	0;		
         	while($Next == true)
 			{
-				echo $paginationlink		=	$pagination.$var;
-				/* $mainpageofprofiles 	=	file_get_html($paginationlink);
+				$paginationlink		=	$pagination.$var;
+				echo $mainpageofprofiles 	=	file_get_html($paginationlink);
 				$var++;
-			if(!$Next)
-			{
-				break;
-			}*/
-
-					
+			
 			}	
 		}
 scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
