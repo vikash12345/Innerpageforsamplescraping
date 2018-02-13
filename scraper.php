eacf7c08959f60f6948f100603e3c9eb1890a394
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-02-13 16:37:35 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -5,19 +5,25 @@
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 //              
-                  $urlofpage  = 'https://indiankanoon.org/search/?formInput=doctypes:bombay%20fromdate:1-12-1902%20todate:%2031-12-1902';
-                  $pagination	=	$urlofpage.'&pagenum=';
-          	  $linkofpage	=	file_get_html($pagination);
+        $urlofpage  = 'https://indiankanoon.org/search/?formInput=doctypes:consumer%20fromdate:1-1-2018%20todate:%2031-1-2018';
+        $pagination	=	$urlofpage.'&pagenum=';
+        $linkofpage	=	file_get_html($pagination);
 		 if($linkofpage)
 		 {	
- 		  	$Next		=	$linkofpage->find("//a[plaintext^=Next]", 0)->plaintext;
-			$NoMATCH	=	$linkofpage->find("//b[plaintext^=No matching results]", 0)->plaintext;
+ 		  	$Next		=	$linkofpage->find("//a[plaintext^=Next]", 0);
+			$NoMATCH	=	$linkofpage->find("//b[plaintext^=No matching results]", 0);
 			$var	=	0;		
-        	while($Next == true)
+        	while($var < 40)
 			{
-				$paginationlink		=	$pagination.$var;
-				echo $mainpageofprofiles 	=	file_get_html($paginationlink);
-				$var++;
+				 $paginationlink		=	$pagination.$var;
+				 $mainpageofprofiles 	=	file_get_html($paginationlink);
+				 $var++;
+				 $resultlink	=	$mainpageofprofiles->find("//a[class='result_url']",0)->href;
+				 echo "$paginationlink\n";
+				if(!$resultlink)
+				{
+					break;
+				}
 			
 			}	
 		}
