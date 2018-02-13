3ce946c8a7a6e2d659df522732a2b26e4ddb3cd5
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-02-13 17:15:16 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -5,7 +5,7 @@
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 //              
-        $urlofpage  = 'https://indiankanoon.org/search/?formInput=doctypes:consumer%20fromdate:1-1-2018%20todate:%2031-1-2018';
+      $urlofpage  = 'https://indiankanoon.org/search/?formInput=doctypes:delhi%20fromdate:1-11-1966%20todate:%2030-11-1966';
         $pagination	=	$urlofpage.'&pagenum=';
         $linkofpage	=	file_get_html($pagination);
 		 if($linkofpage)
@@ -18,8 +18,45 @@ require 'scraperwiki/simple_html_dom.php';
 				 $paginationlink		=	$pagination.$var;
 				 $mainpageofprofiles 	=	file_get_html($paginationlink);
 				 $var++;
-				 $resultlink	=	$mainpageofprofiles->find("//a[class='result_url']",0)->href;
-				 echo "$paginationlink\n";
+				 if($mainpageofprofiles)
+				 {
+					 $resultlink	=	$mainpageofprofiles->find("//div[class='result_url']",0)->plaintext;
+					 
+					 foreach($mainpageofprofiles->find("//div/div/div[@class='result']") as $element)
+					 {
+						//Name of Case
+						$vsname		=	$element->find("//a[@class='result_url']",0)->plaintext;
+						
+						//Link of Case
+						$lvsname		=	$element->find("//a[@class='result_url']",0)->href;
+						
+						//This is for Name of judicary
+						 $courtname	=	$element->find("div[@class='docsource']",0)->plaintext;
+						
+						//Text of Cite
+						$cite	=	$element->find("a[@class='cite_tag']",0)->plaintext;
+						
+						//Link of Cite
+						$lcite	=	$element->find("a[@class='cite_tag']",0)->href;
+						
+						
+						//This is for Full Document	
+						$fulldocument	=	$element->find("//a[plaintext^=Full Document]", 0)->href;
+						
+						 
+			scraperwiki::save_sqlite(array('nameofcase'), array('nameofcase' => $vsname, 
+									    'lvsname' => $lvsname,
+									   'courtname' => $courtname,
+									   'cite' => $cite,
+									   'lcite' => $lcite,
+									   'fulldocument' => $fulldocument));
+		 
+						 
+						
+					 }
+				 	 
+					 	
+				 }
 				if(!$resultlink)
 				{
 					break;
@@ -27,9 +64,7 @@ require 'scraperwiki/simple_html_dom.php';
 			
 			}	
 		}
-scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
-//
-scraperwiki::select("* from data where 'name'='peter'")
+
 
 
 ?>
