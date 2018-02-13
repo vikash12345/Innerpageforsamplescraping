9a1a18c338524b042a0d67c57951ff278c844dbe
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-02-13 12:05:30 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -10,7 +10,7 @@ require 'scraperwiki/simple_html_dom.php';
           	  $linkofpage	=	file_get_html($pagination);
 		  $NoMATCH	=	$linkofpage->find("//b[plaintext^=No matching results]", 0)->plaintext;
 					echo $NoMATCH;
-        				if($linkofpage)
+        				if($NoMATCH)
 					{
 					    $linkofpage
 					}
